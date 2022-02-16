53c4c5da9ac734677ad07250dcaff9cecf36aaa6
dongwook kim <kyaol4@gmail.com>
2022-02-17 02:04:57 +0900

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Every restaurants delete'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164
+  )
+end
+puts 'Finished!'
